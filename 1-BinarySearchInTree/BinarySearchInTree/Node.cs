48777354d3d549710ba2c858ec40d2deec5c1bf9
bmassioui <bouchaib.massioui@gmail.com>
2024-05-03 23:40:43 +0100

    :sparkles: Implement a binary search tree (BST)

---
+++ 1-BinarySearchInTree/BinarySearchInTree/Node.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+namespace BinarySearchInTree;
+
+internal sealed class Node(int value)
+{
+    public int Value { get; init; } = value;
+    public Node? LeftChild { get; private set; }
+    public Node? RightChild { get; private set; }
+
+    /// <summary>
+    /// Attaches a node to the current node as its child.
+    /// </summary>
+    /// <param name="node">The node to attach.</param>
+    /// <exception cref="ArgumentNullException">Thrown when the provided node is null.</exception>
+    public void AttachNode(Node node)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(node));
+
+        bool isDuplicate = node.Value == Value;
+
+        if (isDuplicate) return;
+
+        bool isRightNode = node.Value > Value;
+
+        if (isRightNode)
+        {
+            RightChild = node;
+        }
+        else
+        {
+            LeftChild = node;
+        }
+    }
+
+    /// <summary>
+    /// Searches for a node with the specified value in the tree rooted at the current node.
+    /// </summary>
+    /// <param name="value">The value to search for.</param>
+    /// <returns>The node with the specified value if found; otherwise, null.</returns>>
+    public Node? SearchByValue(int value)
+    {
+        if (value == Value) return this;
+
+        bool shouldSearchInRightChild = value > Value;
+
+        if (shouldSearchInRightChild && RightChild is not null)
+        {
+            return RightChild.SearchByValue(value);
+        }
+
+        bool shouldSearchInLeftChild = value < Value;
+        if (shouldSearchInLeftChild && LeftChild is not null)
+        {
+            return LeftChild.SearchByValue(value);
+        }
+
+        return default;
+    }
+
+    public override string ToString()
+    {
+        bool hasRightChild = RightChild is not null;
+        bool hasLeftChild = LeftChild is not null;
+
+        StringBuilder stringBuilder = new();
+        stringBuilder.Append($"The node value is : {Value}.\n");
+        stringBuilder.Append(hasRightChild ? $"The value of his right child is : {RightChild!.Value}.\n" : "This node has no right child.\n");
+        stringBuilder.Append(hasLeftChild ? $"The value of his left child is : {LeftChild!.Value}.\n" : "This node has no left child.\n");
+
+        return stringBuilder.ToString();
+    }
+}
+
---
+++ 1-BinarySearchInTree/BinarySearchInTree/Program.cs
@@ -0,0 +1,37 @@
+﻿// Binary tree
+
+//          (63)
+//       /      \
+//     (17)      (75)
+//     /  \      / \
+//   (11) (20) (57) (100)
+//                     \
+//                    (149)
+
+
+// 1 - Build the tree
+Node root = new(63);
+Node rightChild1 = new(75);
+Node rightChild2 = new(20);
+Node rightChild3 = new(100);
+Node rightChild4 = new(149);
+
+Node leftChild1 = new(17);
+Node leftChild2 = new(11);
+Node leftChild3 = new(57);
+
+root.AttachNode(leftChild1);
+leftChild1.AttachNode(leftChild2);
+leftChild1.AttachNode(rightChild2);
+
+root.AttachNode(rightChild1);
+rightChild1.AttachNode(leftChild3);
+rightChild1.AttachNode(rightChild3);
+rightChild3.AttachNode(rightChild4);
+
+// 2 - Assert that the node valued at 100 possesses only one right child, specifically the node valued at 149.
+int searchRightChild3 = 100;
+Node? node = root.SearchByValue(searchRightChild3);
+
+// 3 - Print the assert result
+Console.WriteLine(node?.ToString() ?? "No node has been found with the provided search criteria.");
